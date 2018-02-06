9d9b64c28ead2509a66329e8aa12f4292be825fc
Andy Cheng <andyc@cog.systems>
2018-02-06 13:09:46 +1100

    Change startup script to setup for 3 screens

--- i3_startup.sh
+++ i3_startup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xrandr --output DP1 --rotate left --auto --pos 0x0 --output HDMI1 --auto --pos 1440x560
+xrandr --output eDP-1 --auto --pos 0x0 --output DP-1 --auto --pos 1920x0 --output HDMI-1 --auto --pos 4480x0
