e2c7fd995cf450f96c23d1627f214f4d4f83bc53
Andy Cheng <andycheng9018@gmail.com>
2018-10-28 01:02:35 +1100

    Add special cfg for DP 1.2 to HDMI 2.0 converter
    Add special xrandr lines for using the config with
    DP-1.2 to HDMI 2.0 converter. On older intel NUC
    it will not work without this special setting.

--- i3_startup.sh
+++ i3_startup.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 xrandr --output eDP-1 --auto --pos 0x0 --output DP-1 --auto --pos 1920x0 --output HDMI-1 --auto --pos 4480x0
+#xrandr --newmode "clems4k" 534.010 3840 3982 4027 4064 2160 2170 2180 2190 +hsync +vsync
+#xrandr --addmode DP-1 clems4k
+#xrandr --output DP-1 --mode clems4k --scale 0.5x0.5
